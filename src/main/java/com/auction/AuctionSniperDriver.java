94f93a38d6110fd8d1b251e010ce6d0d145005f8
Jason Rogers <jrogers1@ltu.edu>
2020-12-28 20:19:44 -0500

    add auctionsniperdriver.java

---
+++ src/main/java/com/auction/AuctionSniperDriver.java
@@ -0,0 +1,14 @@
+public class AuctionSniperDriver extends JFrameDriver {
+    public AuctionSniperDriver(int timeoutMillis) {
+        super(new GesturePerformer(),
+            JFrameDriver.topLevelFrame(
+                named(Main.MAIN_WINDOW_NAME),
+                showingOnScreen()),
+                new AWTEventQueueProber(timeoutMillis, 100));
+    }
+
+    public void showSniperStatus(String statusText) {
+        new JLabelDriver(
+            this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+    }
+}
